2bb1247ccb1237e7985ebae42cf21f1c1aa77cb0
KK++ <kkplusplus@163.com>
2023-07-10 22:12:57 +0800

    feat: 改用纯js架构

---
+++ test/index.test.js
@@ -0,0 +1,28 @@
+const { run } = require('../src');
+
+test('addition', () => {
+  var operator = [0]
+
+  var numCount = 2;
+  // 运算数范围
+  var range = new Array(
+    new Array(10, 99),
+    new Array(10, 99),
+    new Array(10, 99),
+  );
+  // 运算结果范围
+  var resultRange = new Array(0, 1000);
+
+  // 加法是否进位 0-都可 1-必须进位 2-必须不进位
+  var plusRule = 2;
+  // 减法是否退位 0-都可 1-必须退位 2-必须不退位
+  var minusRule = 2;
+  // 除法是否有余数 0-都可 1-必须有余数 2-必须整除
+  var divideRule = 2;
+
+  // 生成计算题的数量
+  var resultCount = 10;
+
+  run(operator, numCount, range, resultRange, plusRule, minusRule, divideRule, resultCount);
+  
+});
